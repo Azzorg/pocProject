c551b69bbefbe28a7204a1fc0d6f11fc7b4f82d2
Camille Goupil <azzorg.dev@gmail.com>
2019-03-15 16:13:14 +0100

    Add trip creation page

---
+++ src/app/trip-creation/trip-creation.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-creation',
+  templateUrl: './trip-creation.page.html',
+  styleUrls: ['./trip-creation.page.scss'],
+})
+export class TripCreationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
