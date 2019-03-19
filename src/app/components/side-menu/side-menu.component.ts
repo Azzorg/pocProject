5d22784bdac724a6418144d92ef86c4b08dd9292
Camille Goupil <azzorg.dev@gmail.com>
2019-03-19 15:48:30 +0100

    Slider of image on the creation trip page

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
 
 @Component({
@@ -7,6 +7,9 @@ import { IonSlides } from '@ionic/angular';
   styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent implements OnInit {
+  @ViewChild('slideWithMyTrips') slideWithMyTrips: any;
+  @ViewChild('slideWithFriendsTrips') slideWithFriendsTrips: any;
+
   public logo = './../../../assets/logo/logo.png';
 
   public appPages = [
@@ -32,35 +35,9 @@ export class SideMenuComponent implements OnInit {
     }
   ];
 
-  public listTrip = [
-    {
-      name:"Europe Trip",
-      countryImage: "./../../../assets/countries/france.png"
-    },
-    {
-      name:"Rainy London",
-      countryImage: "./../../../assets/countries/uk.png"
-    }
-  ];
+  public listTrip : any;
 
-  public listFriendsTrip = [
-    {
-      avatar:"https://oiseaurose.com/wp-content/uploads/roadtrip-etats-unis-1.jpg",
-      countryImage:"./../../../assets/countries/france.png",
-    },
-    {
-      avatar:"https://www.frommers.com/system/media_items/attachments/000/857/663/s500/1intromoyanbreenf.jpg",
-      countryImage:"./../../../assets/countries/uk.png",
-    },
-    {
-      avatar:"https://www.worldelse.com/wp-content/uploads/2017/10/Road-trip-au-Quebec-87.jpg",
-      countryImage:"./../../../assets/countries/usa.png",
-    },
-    {
-      avatar:"https://www.carnets-de-traverse.com/blog/wp-content/uploads/2015/02/alaska-roadtrip4.jpg",
-      countryImage:"./../../../assets/countries/spain.png",
-    }
-  ];
+  public listFriendsTrip :any;
 
   public user = {
     name : "Azzorg",
@@ -69,18 +46,20 @@ export class SideMenuComponent implements OnInit {
   }
 
   // Options for the slider
-  public slideOpts = {
-    runCallbacksOnInit: true,
-    watchOverflow: true
+  slideOpts = {
+    initialSlide: 0,
+    // runCallbacksOnInit: true,
+    // watchOverflow: true
     //effect: 'fade',
-    //loop: true,
+    loop: true,
     //slidesPerView: 'auto'
   };
 
-  public slideFriendsTripOpts = {
-    runCallbacksOnInit: true,
-    watchOverflow: true,
-    slidesPerView: 'auto',
+  slideFriendsTripOpts = {
+    initialSlide: 0,
+    //runCallbacksOnInit: true,
+    //watchOverflow: true,
+    //slidesPerView: 'auto',
   }
 
   //effect: 'flip',    
@@ -88,6 +67,50 @@ export class SideMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Init the list of the user's trips
+    this.initUserTrips();
+
+    // Init the list of friends' trip
+    this.initListFriendsTrip();
   }
 
+  /**
+   * Initialize the list of the user's trips done currently
+   */
+  initUserTrips(){
+    this.listTrip = [
+      {
+        name:"Europe Trip",
+        countryImage: "./../../../assets/countries/france.png"
+      },
+      {
+        name:"Rainy London",
+        countryImage: "./../../../assets/countries/uk.png"
+      }
+    ];
+  }
+
+  /**
+   * Initialize the list of trip made by the persons followed
+   */
+  initListFriendsTrip(){
+    this.listFriendsTrip = [
+      {
+        avatar:"https://oiseaurose.com/wp-content/uploads/roadtrip-etats-unis-1.jpg",
+        countryImage:"./../../../assets/countries/france.png",
+      },
+      {
+        avatar:"https://www.frommers.com/system/media_items/attachments/000/857/663/s500/1intromoyanbreenf.jpg",
+        countryImage:"./../../../assets/countries/uk.png",
+      },
+      {
+        avatar:"https://www.worldelse.com/wp-content/uploads/2017/10/Road-trip-au-Quebec-87.jpg",
+        countryImage:"./../../../assets/countries/usa.png",
+      },
+      {
+        avatar:"https://www.carnets-de-traverse.com/blog/wp-content/uploads/2015/02/alaska-roadtrip4.jpg",
+        countryImage:"./../../../assets/countries/spain.png",
+      }
+    ];
+  }
 }
--- src/app/trip-creation/trip-creation.page.ts
+++ src/app/trip-creation/trip-creation.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-trip-creation',
@@ -6,10 +6,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip-creation.page.scss'],
 })
 export class TripCreationPage implements OnInit {
+  
+  @ViewChild('slideWithNav2') slideWithNav2: any;
+  
+  sliderTwo: any;
+  
+  slideOptsTwo = {
+    initialSlide: 0,
+    //loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: false
+  };
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+    this.sliderTwo =
+      {
+        isBeginningSlide: true,
+        isEndSlide: false,
+        slidesItems: [
+          {
+            id: 6,
+            image: 'https://media.deseretdigital.com/file/fdd8867843'
+          },
+          {
+            id: 7,
+            image: 'https://www.myutahparks.com/.image/t_share/MTQ4MzM1NjgyNTMwNTE4Nzcy/bryce-amphitheater-inspiration-point_dp_680.jpg'
+          },
+          {
+            id: 8,
+            image: 'https://cdn-image.travelandleisure.com/sites/default/files/1461787471/bryce-canyon-midday-NP0416.jpg'
+          },
+          {
+            id: 9,
+            image: 'https://thumbor.thedailymeal.com/7xRgFvtkSfDB83Uimvk-XpvK7eY=/774x516/https://www.theactivetimes.com/sites/default/files/uploads/m/main-shutterstock_241738303.jpg'
+          },
+          {
+            id: 10,
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg/1200px-USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg'
+          },
+          {
+            id: 11,
+            image: 'https://cdn.rubysinn.com/wp-content/uploads/2014/11/Bryce-canyon-overlook.jpg'
+          }
+        ]
+      };
   }
+
+    //Move to Next slide
+    slideNext(object, slideView) {
+      slideView.slideNext(500).then(() => {
+        this.checkIfNavDisabled(object, slideView);
+      });
+    }
+  
+    //Move to previous slide
+    slidePrev(object, slideView) {
+      slideView.slidePrev(500).then(() => {
+        this.checkIfNavDisabled(object, slideView);
+      });;
+    }
+  
+    //Method called when slide is changed by drag or navigation
+    SlideDidChange(object, slideView) {
+      this.checkIfNavDisabled(object, slideView);
+    }
+  
+    //Call methods to check if slide is first or last to enable disbale navigation  
+    checkIfNavDisabled(object, slideView) {
+      this.checkisBeginning(object, slideView);
+      this.checkisEnd(object, slideView);
+    }
+  
+    checkisBeginning(object, slideView) {
+      slideView.isBeginning().then((istrue) => {
+        object.isBeginningSlide = istrue;
+      });
+    }
+    checkisEnd(object, slideView) {
+      slideView.isEnd().then((istrue) => {
+        object.isEndSlide = istrue;
+      });
+    }
 
 }
