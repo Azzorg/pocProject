b6e41c9bb2a2d8c118f67d82506aac24289a22c6
Camille Goupil <azzorg.dev@gmail.com>
2019-03-27 16:42:16 +0100

    Trash commit

--- src/app/trip-creation/trip-creation.page.ts
+++ src/app/trip-creation/trip-creation.page.ts
@@ -21,7 +21,6 @@ export class TripCreationPage implements OnInit {
   
   slideOpts = {
     initialSlide: 0,
-    //loop: true,
     slidesPerView: 'auto',
     centeredSlides: false
   };
@@ -47,27 +46,21 @@ export class TripCreationPage implements OnInit {
       isEndSlide: false,
       slidesItems: [
         {
-          id: 6,
           image: 'https://media.deseretdigital.com/file/fdd8867843'
         },
         {
-          id: 7,
           image: 'https://www.myutahparks.com/.image/t_share/MTQ4MzM1NjgyNTMwNTE4Nzcy/bryce-amphitheater-inspiration-point_dp_680.jpg'
         },
         {
-          id: 8,
           image: 'https://cdn-image.travelandleisure.com/sites/default/files/1461787471/bryce-canyon-midday-NP0416.jpg'
         },
         {
-          id: 9,
           image: 'https://thumbor.thedailymeal.com/7xRgFvtkSfDB83Uimvk-XpvK7eY=/774x516/https://www.theactivetimes.com/sites/default/files/uploads/m/main-shutterstock_241738303.jpg'
         },
         {
-          id: 10,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg/1200px-USA_10654_Bryce_Canyon_Luca_Galuzzi_2007.jpg'
         },
         {
-          id: 11,
           image: 'https://cdn.rubysinn.com/wp-content/uploads/2014/11/Bryce-canyon-overlook.jpg'
         }
       ]
