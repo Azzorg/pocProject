d1e5ab2f242081ad1d83ecb4c338153feb039715
Camille Goupil <azzorg.dev@gmail.com>
2019-04-03 13:55:27 +0200

    Show only 3 first badges on profil page

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -44,25 +44,25 @@ export class ProfilPage implements OnInit {
   getListBadges(){
     this.listBadge = [
       {
-        name: "Jeune baroudeur",
+        name: "Jeune Baroudeur",
         gotten: true,
         badge:"./../../assets/icon/badges/diamand.svg",
         description:"Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat."
       },
       {
-        name: "Jeune explorateur",
+        name: "Jeune Explorateur",
         gotten: false,
         badge:"./../../assets/icon/badges/star.svg",
         description:"Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis."
       },
       {
-        name:"Grand routeur",
+        name:"Grand Routeur",
         gotten: true,
         badge:"./../../assets/icon/badges/gift.svg",
         description:"Nec piget dicere avide magis hanc insulam populum Romanum invasisse quam iuste. Ptolomaeo enim rege foederato nobis et socio ob aerarii nostri angustias iusso sine ulla culpa proscribi ideoque hausto veneno voluntaria morte deleto et tributaria facta est et velut hostiles eius exuviae classi inpositae in urbem advectae sunt per Catonem, nunc repetetur ordo gestorum."
       },
       {
-        name:"Voyageur de l'extrême",
+        name:"Voyageur de L'Extrême",
         gotten: true,
         badge:"./../../assets/icon/badges/pouce.svg",
         description:"Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur."
