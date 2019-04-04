1fd0ec11f0ae057d581737212edc5e816f3080dd
Camille Goupil <azzorg.dev@gmail.com>
2019-04-04 10:25:46 +0200

    Trips list

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -10,10 +10,13 @@ export class ProfilPage implements OnInit {
 
   user : any;
   listBadge: any;
+  listTrips: any;
   nbKms: number = 11520;
   nbPays: number = 4;
   nbVoyages : number = 6;
   hidden: boolean = true;
+  mapMode: boolean = false;
+
   
 
 
@@ -23,6 +26,7 @@ export class ProfilPage implements OnInit {
   ngOnInit() {
     this.getVisitingProfil();
     this.getListBadges();
+    this.getAllUserTrips();
   }
 
 
@@ -68,6 +72,53 @@ export class ProfilPage implements OnInit {
         description:"Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur."
       }
     ];
+  }
+
+  getAllUserTrips(){
+    this.listTrips = [
+      {
+        name: "Road Trip USA",
+        image: "http://www.lerepairedesmotards.com/img/voyages/etats-unis/colorado_hd.jpg",
+        paysImage: "./../../assets/countries/usa.png",
+        dateDebut: "01/10/2018",
+        dateFin: ""
+      },
+      {
+        name: "Toronto",
+        image: "https://torontosavvy.files.wordpress.com/2015/03/toronto_by_neom.jpg",
+        paysImage: "https://cdn.countryflags.com/thumbs/canada/flag-button-round-250.png",
+        dateDebut: "24/12/2017",
+        dateFin: "02/01/2018"
+      },
+      {
+        name: "Saint Jean de Monts",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/02/1e/dd/93/la-foret-de-pins.jpg",
+        paysImage: "./../../assets/countries/france.png",
+        dateDebut: "20/07/2017",
+        dateFin: "30/07/2017"
+      },
+      {
+        name: "Antibes",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/08/b0/2b/0f/le-sentier-du-littoral.jpg",
+        paysImage: "./../../assets/countries/france.png",
+        dateDebut: "23/08/2016",
+        dateFin: "31/08/2016"
+      },
+      {
+        name: "Humanitaire au Bangladesh",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0e/16/5e/59/rangamati-hanging-bridge.jpg",
+        paysImage: "https://cdn3.iconfinder.com/data/icons/50-flags-of-the-world-circular-shape-1/60/Circular_world_Flag_55-512.png",
+        dateDebut: "10/02/2012",
+        dateFin: "23/02/2012"
+      },
+      {
+        name: "Voyage scolaire Copenhague",
+        image: "https://www.govoyages.com/blog/wp-content/uploads/sites/14/2018/10/deux-jours-copenhague-720.jpg",
+        paysImage: "https://banner2.kisspng.com/20180608/hyl/kisspng-flag-of-denmark-flag-of-luxembourg-danish-flag-of-5b1a099f7c96f4.4275827715284330555103.jpg",
+        dateDebut: "05/05/2011",
+        dateFin: "18/05/2012"
+      }
+    ]
   }
 
 
