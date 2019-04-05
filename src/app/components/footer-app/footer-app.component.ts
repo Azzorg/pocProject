0af79a9b0a10793dd485ada69fac8b84be323895
Camille Goupil <azzorg.dev@gmail.com>
2019-04-05 11:47:46 +0200

    Animation css to show adding options

--- src/app/components/footer-app/footer-app.component.ts
+++ src/app/components/footer-app/footer-app.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-app.component.scss'],
 })
 export class FooterAppComponent implements OnInit {
+  showOptions: boolean = false;
 
   constructor() { }
 
   ngOnInit() {}
 
-  clickMainFAB(){
-    console.log('Clicked open social menu');
+  /**
+   * Show or hide adding options
+   */
+  toggleAddOptions(){
+    this.showOptions = !this.showOptions;
   }
 
 }
