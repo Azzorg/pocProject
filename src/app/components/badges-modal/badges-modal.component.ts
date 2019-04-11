8ab2cc931b2346c8169c1ebd16ceaa514e8013bc
Azzorg <azzorg.dev@gmail.com>
2019-04-11 17:11:43 +0200

    Animation to display either the list of earned badges or the list of in progress earning badges

--- src/app/components/badges-modal/badges-modal.component.ts
+++ src/app/components/badges-modal/badges-modal.component.ts
@@ -11,15 +11,34 @@ export class BadgesModalComponent implements OnInit {
     // "badge" passed in componentProps
     @Input() badge: any;
 
+    showEarnedBadges: boolean = true;
+
   constructor(private modalCtrl:ModalController) { }
 
   ngOnInit() {}
+
 
   /**
    * To dismiss the modal
    */
   dismiss(){
     this.modalCtrl.dismiss();
+  }
+
+
+  /**
+   * Go to in progress badges list : "swiping" on left
+   */
+  goToInProgressBadgesList(){
+    this.showEarnedBadges = false;
+  }
+
+
+  /**
+   * Go to earned badges list : "swiping" on right
+   */
+  goToEarnedBadgesList(){
+    this.showEarnedBadges = true;
   }
 
 }
