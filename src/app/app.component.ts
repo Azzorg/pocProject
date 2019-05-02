9ba155a3ffb2cf1ac2f58ffe244bc84f3a845109
Azzorg <azzorg.dev@gmail.com>
2019-05-02 15:54:15 +0200

    Sidemenu : 2 looks of the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { Platform, Events } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
