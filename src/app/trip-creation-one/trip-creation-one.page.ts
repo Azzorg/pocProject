3d99c0640c9f940bf3168f725245d9e68fb7bc85
Azzorg <azzorg.dev@gmail.com>
2019-05-07 16:57:52 +0200

    Trip creation one page : Adding elements on html and click actions

--- src/app/trip-creation-one/trip-creation-one.page.ts
+++ src/app/trip-creation-one/trip-creation-one.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-trip-creation-one',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripCreationOnePage implements OnInit {
 
+  // Variables from model
+  tripName: string;
+  startDate: Date;
+  endDate: Date;
+  visibility: string;
+  recit: string;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  /**
+   * Next click action to the second page of trip creation 
+   */
+  goNext(){
+    console.log("Go to the second page to create a new trip");
+    console.log("Nom du voyage : " + this.tripName);
+    console.log("Date de début du voyage : " + this.startDate);
+    console.log("date de fin du voyage : " + this.endDate);
+    console.log("Visibilité du voyage : " + this.visibility);
+    console.log("Récit du voyage : " + this.recit);
+  }
+
+
+  /**
+   * Return to the 
+   */
+  cancelButton(){
+    console.log("Click on cancel button");
   }
 
 }
